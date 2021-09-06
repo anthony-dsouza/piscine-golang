d7efea36ff4d4df1102e8e14b1c952030cfbb132
Adsouza <anthonydsouza58@gmail.com>
2021-09-06 13:30:14 +0100

    id

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,3 @@
-curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"Adsouza\"}}){id}}"}' 
+#! /bin/bash/
+
+curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"Adsouza\"}}){id}}"}' | cut -d "}" -f 1 | cut -b 24-27
