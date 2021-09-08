551f4de88ed45aeadb111328f681d9fc1c5dd86f
Adsouza <anthonydsouza58@gmail.com>
2021-09-08 13:40:48 +0100

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -1,37 +1,26 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintComb() {
-	a := 0
-	b := 0
-	c := 0
-
-	for a <= 9 {
-		if c == 7 {
-			z01.PrintRune('\n')
-			a = 10
-		} else {
-			if a == 9 {
-				if b == 8 && c < 7 {
-					c++
-					b = c + 1
-					a = b + 1
-				} else if b < 8 {
-					b++
-					a = b + 1
+	for a := 0; a < 10; a++ {
+		for b := 0; b < 10; b++ {
+			for c := 0; c < 10; c++ {
+				if a == 7 && b == 8 && c == 9 {
+					z01.PrintRune(rune(a) + 48)
+					z01.PrintRune(rune(b) + 48)
+					z01.PrintRune(rune(c) + 48)
+					z01.PrintRune('\n')
+				} else if a < b && b < c {
+					z01.PrintRune(rune(a) + 48)
+					z01.PrintRune(rune(b) + 48)
+					z01.PrintRune(rune(c) + 48)
+					z01.PrintRune(44)
+					z01.PrintRune(32)
 				}
-			} else {
-				a++
 			}
-			// comma
-			z01.PrintRune(44)
-			// space
-			z01.PrintRune(32)
-			z01.PrintRune(rune(c) + 48)
-			z01.PrintRune(rune(b) + 48)
-			z01.PrintRune(rune(a) + 48)
 		}
 	}
-
 }
