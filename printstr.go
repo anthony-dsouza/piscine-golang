2ab508d31bd8b325b7832e74c759d1181961f555
Adsouza <anthonydsouza58@gmail.com>
2021-09-09 10:19:16 +0100

    printstr

---
+++ printstr.go
@@ -0,0 +1,17 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintStr(s string) {
+	a := []byte(s)
+
+	b := len(a)
+
+	for i := 0; i < b; i++ {
+		j := rune(a[i])
+		z01.PrintRune(j)
+	}
+	z01.PrintRune('\n')
+}
