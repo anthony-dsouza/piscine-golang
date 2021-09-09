7e79488b25ca2e1efdfd6abea539b4bd6aafda76
Adsouza <anthonydsouza58@gmail.com>
2021-09-09 10:28:29 +0100

    printstrv2

--- printstr.go
+++ printstr.go
@@ -7,10 +7,14 @@ import (
 func PrintStr(s string) {
 	a := []byte(s)
 
-	b := len(a)
+	// b := len(a)
 
-	for i := 0; i < b; i++ {
-		j := rune(a[i])
-		z01.PrintRune(j)
+	// for i := 0; i < b; i++ {
+	// 	j := rune(a[i])
+	// 	z01.PrintRune(j)
+	// }
+
+	for _, letter := range a {
+		z01.PrintRune(rune(letter))
 	}
 }
