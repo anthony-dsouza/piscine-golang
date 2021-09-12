0a19aebc804f3b7cca20505ed1c4fd558a1db3b0
Adsouza <anthonydsouza58@gmail.com>
2021-09-12 14:29:18 +0100

    1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,10 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb <= 0 || nb > 24 {
+	if nb < 0 || nb > 24 {
 		return 0
+	} else if nb == 0 {
+		return 1
 	} else {
 		result := nb
 		for i := 1; i < nb; i++ {
