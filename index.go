1365514bdb068282f7edd2c6ecfef5d3a96f6464
Adsouza <anthonydsouza58@gmail.com>
2021-09-13 18:33:22 +0100

    index

--- index.go
+++ index.go
@@ -3,11 +3,20 @@ package piscine
 func Index(s string, toFind string) int {
 	find := []rune(toFind)
 	length := len(find)
+	str := []rune(s)
+	s_len := len(str)
+
+	if length <= 0 {
+		return -1
+	}
 
 	for i, v := range s {
 		if v == find[0] {
-			if s[i:length+i] == toFind {
-				return i
+			if length+i > s_len {
+			} else {
+				if s[i:length+i] == toFind {
+					return i
+				}
 			}
 		}
 	}
