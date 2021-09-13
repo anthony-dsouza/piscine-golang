59104f5d181e42beee621074f03cde013f229f0d
Adsouza <anthonydsouza58@gmail.com>
2021-09-13 10:46:11 +0100

    1

--- sqrt.go
+++ sqrt.go
@@ -1,7 +1,10 @@
 package piscine
 
 func Sqrt(nb int) int {
-	for i := 1; i < nb; i++ {
+	if nb == 1 {
+		return 1
+	}
+	for i := 0; i < nb; i++ {
 		if nb%i > 0 {
 		} else if i == 1 {
 		} else if i*i == nb {
