3075cff4fa5ed46e6d47f1a29812d776fa2946c6
Adsouza <anthonydsouza58@gmail.com>
2021-09-16 16:35:49 +0100

    noargs

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -18,33 +18,38 @@ func main() {
 		upper = append(upper, i)
 	}
 
-	if os.Args[1] == "--upper" {
-		for _, v := range os.Args[2:] {
-			value := 0
-			for _, a := range v {
-				c := int(a) - 48
-				value = (value * 10) + c
-			}
-			if value > 26 || value <= 0 {
-				z01.PrintRune(32)
-			} else {
-				z01.PrintRune(upper[value-1])
-			}
+	l := len(os.Args)
 
-		}
+	if l == 1 {
 	} else {
-		for _, v := range os.Args[1:] {
-			value := 0
-			for _, a := range v {
-				c := int(a) - 48
-				value = (value * 10) + c
+		if os.Args[1] == "--upper" {
+			for _, v := range os.Args[2:] {
+				value := 0
+				for _, a := range v {
+					c := int(a) - 48
+					value = (value * 10) + c
+				}
+				if value > 26 || value <= 0 {
+					z01.PrintRune(32)
+				} else {
+					z01.PrintRune(upper[value-1])
+				}
+
 			}
-			if value > 26 || value <= 0 {
-				z01.PrintRune(32)
-			} else {
-				z01.PrintRune(lower[value-1])
+		} else {
+			for _, v := range os.Args[1:] {
+				value := 0
+				for _, a := range v {
+					c := int(a) - 48
+					value = (value * 10) + c
+				}
+				if value > 26 || value <= 0 {
+					z01.PrintRune(32)
+				} else {
+					z01.PrintRune(lower[value-1])
+				}
 			}
 		}
+		z01.PrintRune('\n')
 	}
-	z01.PrintRune('\n')
 }
