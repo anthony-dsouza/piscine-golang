9221264ebed2a7c7497109ba3104a9499bc63a38
Adsouza <anthonydsouza58@gmail.com>
2021-09-16 16:09:08 +0100

    nbrcovertalpha

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	var lower []rune
+	var upper []rune
+
+	for i := 'a'; i <= 'z'; i++ {
+		lower = append(lower, i)
+	}
+
+	for i := 'A'; i <= 'Z'; i++ {
+		upper = append(upper, i)
+	}
+
+	if os.Args[1] == "--upper" {
+		for _, v := range os.Args[2:] {
+			value := 0
+			for _, a := range v {
+				c := int(a) - 48
+				value = (value * 10) + c
+			}
+
+			z01.PrintRune(upper[value-1])
+		}
+	} else {
+		for _, v := range os.Args[1:] {
+			value := 0
+			for _, a := range v {
+				c := int(a) - 48
+				value = (value * 10) + c
+			}
+
+			z01.PrintRune(lower[value-1])
+		}
+	}
+}
