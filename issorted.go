384f4a9ef3aec6127aec5b421d4f21d1096ad13a
Adsouza <anthonydsouza58@gmail.com>
2021-09-20 17:06:41 +0100

    issorted

---
+++ issorted.go
@@ -0,0 +1,27 @@
+package piscine
+
+func Sign(a, b int) int {
+	if a == b {
+		return 0
+	} else if a > b {
+		return 1
+	} else {
+		return -1
+	}
+}
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	l := len(a) - 1
+	for i := range a {
+		if i < l-1 {
+			if f(a[i], a[i+1]) == 0 {
+				continue
+			} else if f(a[i], a[i+1]) == 1 {
+				return false
+			} else if f(a[i], a[i+1]) == -1 {
+				continue
+			}
+		}
+	}
+	return true
+}
