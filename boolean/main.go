179cb7928a1fabef4e8ba88c89a500d01da3a991
Adsouza <anthonydsouza58@gmail.com>
2021-09-20 10:23:24 +0100

    boolean

---
+++ boolean/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func Even(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n%2 == 0 {
+		return 1
+	}
+	return 0
+}
+
+func isEven(nbr int) bool {
+	if Even(nbr) == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	lengthOfArg := len(os.Args[1:])
+	EvenMsg := "I have an number even of arguements"
+	OddMsg := "I have an odd number of arguements"
+	if isEven(lengthOfArg) {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
