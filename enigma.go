7926e97db2b2d28240ed26bd7c341bbe787624ba
Adsouza <anthonydsouza58@gmail.com>
2021-09-22 14:21:41 +0100

    enigma

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	ac := ***a
+	cd := *******c
+	db := ****d
+	ba := *b
+
+	*******c = ac
+	****d = cd
+	*b = db
+	***a = ba
+}
