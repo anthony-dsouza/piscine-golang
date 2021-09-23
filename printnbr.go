7bb93f7a06678757e324801b542fd7ddf8d0febc
Adsouza <anthonydsouza58@gmail.com>
2021-09-23 12:54:48 +0100

    nrb

--- printnbr.go
+++ printnbr.go
@@ -2,11 +2,29 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintNbr(nbr int) {
-	aRune := rune(nbr + 48)
-	z01.PrintRune(aRune)
-}
+func PrintNbr(n int) {
+	var str string
+	neg := false
 
-func Println() {
-	z01.PrintRune('\n')
+	if n < 0 {
+
+		n = n * -1
+		neg = true
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+	} else {
+		for n > 0 {
+			str = string(rune(n%10+48)) + str
+			n = n / 10
+		}
+	}
+
+	if neg {
+		z01.PrintRune('-')
+	}
+
+	for _, v := range str {
+		z01.PrintRune(v)
+	}
 }
