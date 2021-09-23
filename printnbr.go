25a11a42de78423e7fd901dc0eb29fc206e891e4
Adsouza <anthonydsouza58@gmail.com>
2021-09-23 13:27:25 +0100

    asd

--- printnbr.go
+++ printnbr.go
@@ -1,14 +1,20 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbr(n int) {
 	var str string
 	neg := false
+	overflow := false
+	ov := 0
 
 	if n < 0 {
 		if n == -9223372036854775808 {
-			n = -9223372036854775807
+			ov = (n % 10) * -1
+			n = n / 10
+			overflow = true
 		}
 		n = n * -1
 		neg = true
@@ -20,6 +26,10 @@ func PrintNbr(n int) {
 			str = string(rune(n%10+48)) + str
 			n = n / 10
 		}
+	}
+
+	if overflow {
+		str = str + string(rune(ov+48))
 	}
 
 	if neg {
